bc740a3a52371b609cb9a902b1c4c57e9bc914f8
sabika <aunaliwasaya606@gmail.com>
2025-04-02 02:34:27 +0500

    added codeup logo

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Home, Info, Phone, Briefcase, Circle } from "lucide-react";
+import { Home, Info, Phone, Briefcase } from "lucide-react";
+import codeuplogo from "../assets/codeup-favicon.png";
 
 function BottomNav() {
   const isDarkMode = useSelector((state) => state.theme.isDarkMode);
-
+  
   return (
     <div
       className={`fixed bottom-0 left-0 m-1 w-[98%] p-3 md:hidden rounded-lg flex justify-around items-center shadow-md border 
         backdrop-filter backdrop-blur-lg
         ${isDarkMode 
           ? "bg-white bg-opacity-80 text-blue-600 border-gray-300" 
-          : "bg-black bg-opacity-80 text-white border-gray-200"}`}
+          : "bg-black bg-opacity-80 text-gray-700 border-gray-200"}`}
     >
       {/* Home */}
       <div className="flex flex-col items-center hover:text-blue-500 transition">
@@ -27,7 +28,11 @@ function BottomNav() {
 
       {/* Logo in the center */}
       <div className="flex flex-col items-center">
-        <Circle size={30} />
+        {/* Replace with your image path */}
+    
+        <a href="https://codeup.solutions/" target="_blank" rel="noopener noreferrer">
+  <img src={codeuplogo} alt="Codeup Logo" className="w-12 h-12" />
+</a>
       </div>
 
       {/* Contact */}
